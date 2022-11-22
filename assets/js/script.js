e63fb6984dd963146212137b8f17ef095015b2f3
JaspalSJ <jabbaljaspal@gmail.com>
2022-11-22 18:40:51 +0000

    Add additional code for shuffling emojis

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,20 @@ const state = {
 
 /** Code to shuffle the emojis */
 
+const shuffle = array => {
+    const clonedArray = [...array];
+
+    for (let index = clonedArray.length - 1; index > 0; index--) {
+        const randomIndex = Math.floor(Math.random() * (index + 1));
+        const original = clonedArray[index];
+
+        clonedArray[index] = clonedArray[randomIndex];
+        clonedArray[randomIndex] = original;
+    }
+
+    return clonedArray;
+};
+
 const pickRandom = (array, items) => {
     const clonedArray = [...array];
     const randomPicks = [];
